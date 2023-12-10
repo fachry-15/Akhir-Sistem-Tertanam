b3714b30992d52d6171c51ccffac24d3fb71d03c
Hula-Hula <fachry150503@gmail.com>
2023-12-10 20:37:17 +0700

    fixingxls

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import pandas as pd
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 
-# Data dummy langsung dalam bentuk DataFrame Pandas
-data_dummy = pd.DataFrame({
-    'Alternatif': ['A', 'B', 'C', 'D', 'E'],
-    'Kriteria1': [4, 2, 6, 8, 5],
-    'Kriteria2': [7, 5, 8, 6, 4],
-    'Kriteria3': [5, 8, 4, 7, 6],
-    'Kriteria4': [8, 6, 7, 5, 9]
-})
-
-# Menampilkan data dummy
-print("\nData Dummy:")
-print(tabulate(data_dummy, headers='keys', tablefmt='fancy_grid'))
-
-# Normalisasi matriks keputusan
 def normalize(df):
     result = df.copy()
     for feature_name in df.columns[1:]:
@@ -24,32 +10,69 @@ def normalize(df):
         result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
 
-# Menghitung bobot normalisasi
-normalized_data = normalize(data_dummy)
-weights = [0.25, 0.25, 0.25, 0.25]  # Atur bobot sesuai kebutuhan
-weighted_data = normalized_data.copy()
-for i, feature_name in enumerate(normalized_data.columns[1:]):
-    weighted_data[feature_name] = normalized_data[feature_name] * weights[i]
+def save_to_excel(sorted_data):
+    file_path_output = 'HasilPeringkat.xlsx'  # Nama file untuk menyimpan hasil peringkat
 
-# Menampilkan hasil normalisasi
-print("\nMatriks Normalisasi:")
-print(tabulate(normalized_data, headers='keys', tablefmt='fancy_grid'))
+    # Ubah nama kolom
+    sorted_data = sorted_data.rename(columns={'Alternatif': 'Nama Mahasiswa', 'Total': 'Nilai Akhir Perangkingan'})
 
-# Menampilkan bobot normalisasi
-print("\nBobot Normalisasi:")
-print(tabulate(weighted_data, headers='keys', tablefmt='fancy_grid'))
+    sorted_data.to_excel(file_path_output, index=False)
+    print(f"\nHasil peringkat disimpan ke dalam file: {file_path_output}")
 
-# Menghitung nilai akhir untuk setiap alternatif
-weighted_data['Total'] = weighted_data.iloc[:, 1:].sum(axis=1)
+def main():
+    # Membaca data dari file Excel
+    file_path = 'Daftar Pendaftar Beasiswa.xlsx'  # Ganti dengan lokasi dan nama file Anda
+    data_excel = pd.read_excel(file_path)
 
-# Menampilkan hasil akhir
-print("\nHasil Akhir:")
-print(tabulate(weighted_data[['Alternatif', 'Total']].sort_values(by='Total', ascending=False), headers='keys', tablefmt='fancy_grid'))
+    # Menampilkan data dari file Excel
+    print("\nData dari File Excel:")
+    print(tabulate(data_excel, headers='keys', tablefmt='fancy_grid'))
 
-# Visualisasi hasil menggunakan grafik batang
-plt.figure(figsize=(8, 5))
-plt.bar(weighted_data['Alternatif'], weighted_data['Total'], color='skyblue')
-plt.title('Peringkat Hasil Akhir SAW')
-plt.xlabel('Alternatif')
-plt.ylabel('Nilai Total')
-plt.show()
+    # Normalisasi matriks keputusan
+    normalized_data = normalize(data_excel)
+    weights = [0.2, 0.2, 0.2, 0.2, 0.2]  # Atur bobot sesuai kebutuhan
+    weighted_data = normalized_data.copy()
+    for i, feature_name in enumerate(normalized_data.columns[1:]):
+        weighted_data[feature_name] = normalized_data[feature_name] * weights[i]
+
+    # Menampilkan hasil normalisasi
+    print("\nMatriks Normalisasi:")
+    print(tabulate(normalized_data, headers='keys', tablefmt='fancy_grid'))
+
+    # Menampilkan bobot normalisasi
+    print("\nBobot Normalisasi:")
+    print(tabulate(weighted_data, headers='keys', tablefmt='fancy_grid'))
+
+    # Menghitung nilai akhir untuk setiap alternatif
+    weighted_data['Total'] = weighted_data.iloc[:, 1:].sum(axis=1)
+
+    # Menambahkan kolom 'Alternatif' ke DataFrame weighted_data
+    if 'Alternatif' in data_excel.columns:
+        weighted_data['Alternatif'] = data_excel['Alternatif']
+    else:
+        print("Kolom 'Alternatif' tidak ditemukan dalam data. Pastikan nama kolomnya benar.")
+
+    if 'Total' in weighted_data.columns:
+        # Menampilkan hasil akhir
+        sorted_weighted_data = weighted_data[['Alternatif', 'Total']].sort_values(by='Total', ascending=False)
+        print("\nHasil Akhir:")
+        print(tabulate(sorted_weighted_data, headers='keys', tablefmt='fancy_grid'))
+
+        # Simpan hasil peringkat ke file Excel
+        save_to_excel(sorted_weighted_data)
+
+        # Visualisasi hasil menggunakan grafik batang
+        plt.figure(figsize=(15, 10))
+        plt.bar(sorted_weighted_data['Alternatif'], sorted_weighted_data['Total'], color='skyblue')
+        plt.title('Peringkat Hasil Akhir SAW')
+        plt.xlabel('Alternatif')
+        plt.ylabel('Nilai Total')
+        plt.xticks(rotation=45, ha='right') 
+        plt.show()
+    else:
+        print("Kolom 'Total' tidak ditemukan dalam data. Pastikan perhitungan nilai akhir sudah benar.")
+        
+        
+
+if __name__ == "__main__":
+    main()
